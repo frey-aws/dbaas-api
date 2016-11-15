28771948b55b19df73e961c9f77ea7be3a40701e
Dennis Frey <freydenn@amazon.com>
2016-11-14 23:59:10 -0500

    Harness update

--- OperationsApi.Harness/Program.cs
+++ OperationsApi.Harness/Program.cs
@@ -9,13 +9,18 @@ namespace OperationsApi.Harness
     {
         static void Main(string[] args)
         {
-            AmazonRdsCommand cmd = new AmazonRdsCommand();            
-            
-            for(int x = 0; x <= 100; x++)
-            {                
+            SimpleThreadLoggingTest();           
+        }
+
+        // Really simple test, yes has threading issues, but confirms that the logging will be properly threading, albeit not perfect yet
+        private static void SimpleThreadLoggingTest()
+        {
+            AmazonRdsCommand cmd = new AmazonRdsCommand();
+
+            for (int x = 0; x <= 100; x++)
+            {
                 Task.Run(() => { cmd.JustLog("Test " + Interlocked.Increment(ref x)); });
-                Thread.Sleep(1000);               
-            }            
+            }
         }
     }
 }
